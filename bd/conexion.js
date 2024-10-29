5af5912d94fb0031f98683b5a873f8ec84bb0172
Omar-R3 <joseomargarciacruz@gmail.com>
2024-10-28 22:42:05 -0600

    first commit

---
+++ bd/conexion.js
@@ -0,0 +1,21 @@
+const admin = require("firebase-admin");
+const keys = require("../Keys.json");
+admin.initializeApp({
+    credential: admin.credential.cert(keys)
+});
+
+const proyecto = admin.firestore();
+
+const usuarios = proyecto.collection("miejemploBD");
+const productos = proyecto.collection("productos");
+const ventas = proyecto.collection("ventas");
+//const productos = proyecto.collection("productos");
+
+//console.log(usuarios);
+
+
+module.exports = {
+    usuarios,
+    productos,
+    ventas
+}
